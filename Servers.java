7fd2f274f876dd2240e3525e529e279d756cbdf5
WenmingZH <81204835+WenmingZH@users.noreply.github.com>
2021-05-23 18:15:55 +1000

    Add files via upload

---
+++ Servers.java
@@ -0,0 +1,9 @@
+public class Servers{
+	public String serverName;
+	public int serverId;
+	public String state;
+	public int currStartTime;
+	public int cores;
+	public int nem;
+	public int disk;
+}
